9e1db4ee9755c16a3e2bca3a0e9003e413d654e3
Benmuiruri <ben.muiruri13@gmail.com>
2022-03-09 07:11:18 -0500

    Add mobile modal in styles css, create speaker list in main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,65 @@
-// get elements from html
+// List of speaker objects
 
-const openModal = document.querySelector('#mobile-menu');
-const closeModal = document.querySelector('#close-menu');
+const speaker1 = {
+  imageSrc: '../images/ross-brawn.jpg',
+  speakerName: 'Ross Brawn',
+  title: '',
+  speakerDescription: '',
+};
+const speaker2 = {
+  imageSrc: '../images/muhammed.png',
+  speakerName: 'Mohammed Ben Sulayem',
+  title: '',
+  speakerDescription: '',
+};
+const speaker3 = {
+  imageSrc: '../images/wolff.jpg',
+  speakerName: 'Toto Wolff',
+  title: '',
+  speakerDescription: '',
+};
+const speaker4 = {
+  imageSrc: '../images/christian-horner-2021.jpg',
+  speakerName: 'Christian Horner',
+  title: '',
+  speakerDescription: '',
+};
+const speaker5 = {
+  imageSrc: '../images/../images/lewis.jpg',
+  speakerName: 'Lewis Hamilton',
+  title: '',
+  speakerDescription: '',
+};
+const speaker6 = {
+  imageSrc: '../images/sebastian-vettel.jpg',
+  speakerName: 'Sebastian Vettel',
+  title: '',
+  speakerDescription: '',
+};
+
+const speakers = [speaker1, speaker2, speaker3, speaker4, speaker5, speaker6];
+// Target open and close triggers in html
+
+const openEls = document.querySelectorAll('[data-open]');
+const closeEls = document.querySelectorAll('[data-close]');
+const openElems = Array.from(openEls);
+const closeElems = Array.from(closeEls);
 const isVisible = 'is-visible';
 const navLinks = document.querySelectorAll('.web-header-link');
 
-// add event listener to the hamburger menu, add make modal visible on page and disable scroll
-openModal.addEventListener('click', function () {
-  const modalId = this.dataset.open;
-  document.getElementById(modalId).classList.add(isVisible);
-  document.body.style.overflow = 'hidden';
+openElems.forEach((el) => {
+  el.addEventListener('click', function () {
+    const modalId = this.dataset.open;
+    document.getElementById(modalId).classList.add(isVisible);
+    document.body.style.overflow = 'hidden';
+  });
 });
-
-// add event listener on x button to close modal and enable scroll
-closeModal.addEventListener('click', function () {
-  this.parentElement.parentElement.classList.remove(isVisible);
-  document.body.style.overflow = 'auto';
+// Close modal
+closeElems.forEach((el) => {
+  el.addEventListener('click', function () {
+    this.parentElement.parentElement.classList.remove(isVisible);
+    document.body.style.overflow = 'auto';
+  });
 });
 
 // add event listener on click outside modal to close modal and enable scroll
@@ -41,3 +85,6 @@ navLinks.forEach((elem) => {
     document.body.style.overflow = 'auto';
   });
 });
+
+// Create Speaker Section HTML 
+
